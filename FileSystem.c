932c464d327bb454aa5d31da1c4b7b46ead1d7cf
Ivodan <ivandgomezy@gmail.com>
2017-06-07 16:55:48 -0300

    dummy

--- FileSystem.c
+++ FileSystem.c
@@ -1,113 +1,149 @@
+#define clear() printf("\033[H\033[J")
+#define VALIDARARCHIVO 0
+#define CREARARCHIVO 1
+#define BORRARARCHIVO 2 
+#define OBTENERDATOS 3
+#define GUARDARDATOS 4
+#include "estructuras.h"
+#include "funciones.h"
+#include <commons/collections/list.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <commons/txt.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stddef.h>
+#include <math.h>
+#include <stdint.h>
 #include <commons/config.h>
-// #include "desSerializador.h"
-#include "../Kernel/estructuras.h"
+#define FILESYSTEM 84
+#define BACKLOG 5
+#define LIBRE 0
+#define OCUPADO 1
+#define KERNEL 0
+	#define ARRAYPIDS 5
+	#define PIDFINALIZACION 2
+	#define PATH 3
+	#define PIDINFO 4
+	#define RESPUESTAOKMEMORIA 1
 
-// if ((header.seleccionador).unaInterfaz==3)
-// {
-// 	switch(tipoPaquete){
-// 		case LEER:
-// 		break;
-// 		case ESCRIBIR:
-// 		break;
-		
-// 	}
-// }
+	#define SOLICITUDMEMORIA 0
+	#define SOLICITUDINFOPROG 1
+	#define ESCRIBIRMEMORIA 2
+	#define LIBERARMEMORIA 3
+ 	#define ACTUALIZARPCB 4
+	
+	//-------------------------------
+	#define DELAY 0
+	#define DUMP 1
+	#define FLUSH 2
+	#define SIZE 3
+#define CONSOLA 2
+	#define INICIARPROGRAMA 0
+	#define FINALIZARPROGRAMA 1
+	#define DESCONECTARCONSOLA 2
+	#define LIMPIARMENSAJES 3
+	//------------------------------	
+	#define MENSAJES 0
+	#define PIDNUEVO 1
+
+#define CPU 3
+#define FS 4
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define FAIL 0
+#define BLOQUE 20
+
+#define CACHE 0
+#define MEMORIA 1 
+#define TABLA 2
+#define PID 3
+#define PCB 3
+#define PIDSIZE 1
+#define MEMORIASIZE 0
 
 int main(){
-	/* LEER CONFIGURACION
-	*
-	*/
+struct sockaddr_in addr;
+int addrlen= sizeof(addr);
+int socketNuevaConexion,rv;
+struct addrinfo hints;
+struct addrinfo *serverInfo;
+int listenningSocket;
+int * unBuffer=malloc(sizeof(int));
+t_seleccionador * seleccionador=malloc(sizeof(t_seleccionador));
+
+memset(&hints, 0, sizeof(hints));
+hints.ai_family = AF_INET;
+hints.ai_flags = AI_PASSIVE;
+hints.ai_socktype = SOCK_STREAM;
+
+if ((rv =getaddrinfo(NULL, "5003", &hints, &serverInfo)) != 0) fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+
+listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
+sleep(1);
+fflush(stdout);
+printf("%s \n", "Socket OK");
+
+if(bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen)==-1) {perror("Error en el bind."); exit(1);}
+
+fflush(stdout);
+sleep(1);
+printf("%s \n", "Bind OK\n");
+printf("%s \n", "El Servidor se encuentra OK para escuchar conexiones.");
+
+freeaddrinfo(serverInfo);
+sleep(1);
+
+while((socketNuevaConexion = accept(listenningSocket, (struct sockaddr *)&addr,&addrlen)) < 0);
+		handshakeServer(socketNuevaConexion,FILESYSTEM,(void*)unBuffer);
+
+while(1) {
+while(0>recv(socketNuevaConexion,seleccionador,sizeof(t_seleccionador),0));
 	
-
-
-	t_config *CFG;
-	CFG = config_create("fileSystemCFG.txt");
-	char *PUERTO= config_get_string_value(CFG ,"PUERTO");
-	char *PUNTO_MONTAJE= config_get_string_value(CFG ,"PUNTO_MONTAJE");
-	char *IP= config_get_string_value(CFG ,"IP");
-	printf("Configuración:\nPUERTO = %s,\nPUNTO_MONTAJE = %s,\nIP = %s.\n",PUERTO,PUNTO_MONTAJE,IP);
-		
-	printf("Presione enter para continuar.\n");
-	getchar();
-	/*
-	*
-	*/
-int bytesRecibidos,iof;
-char* message=malloc(100*sizeof(char));
-char *handshakeCliente="Hola soy el fileSystem.";
-	struct addrinfo hints;
-	struct addrinfo *serverInfo;
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;		
-	hints.ai_socktype = SOCK_STREAM;	
-
-	getaddrinfo(IP, "5002", &hints, &serverInfo);	
-
-
-
-	int serverSocket;
-	int flagHandshake=1;
-	serverSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
-
-
-	connect(serverSocket, serverInfo->ai_addr, serverInfo->ai_addrlen);
-
-
-
-	freeaddrinfo(serverInfo);
-
-	config_destroy(CFG);
-
-	int enviar = 1;
-
-	char *messageRecv;
-
-	messageRecv = malloc(100*(sizeof(char)));
-
-	while(flagHandshake){bytesRecibidos=recv(serverSocket,messageRecv,100*sizeof(char),0);
-	if(bytesRecibidos>0){flagHandshake=0;fflush(stdout); printf("%s\n", messageRecv);send(serverSocket, handshakeCliente, 100*sizeof(char), 0);}
-	}
-
-	while(enviar){
-
-
-		if ((bytesRecibidos=recv(serverSocket,messageRecv,100*sizeof(char),0)) == -1){
-
-			      // llamada a recv()						perror("recv");
-			      exit(-1);
-						fflush(stdout);
-			     	   }
-
-	    printf("%s\n", messageRecv);
-
-	    /*scanf("%s", message);			// Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
-		if (!strcmp(message,"exit\n")) enviar = 0;			// Chequeo que el usuario no quiera salir
-		if (enviar) send(serverSocket, message,100*sizeof(char), 0); 	// Solo envio si el usuario no quiere salir.
-	/*
-	 *
-	 */}
-
-	free(message);
-	free(messageRecv);
-
-
-	close(serverSocket);
-	return 0;
-
-
+	switch (seleccionador->tipoPaquete){
+		case VALIDARARCHIVO: printf("%s \n", "Validado, troesma.\n");
+		/*Parámetros: [Path]
+	    validar que el archivo exista*/
+		break;
+		case CREARARCHIVO: printf("%s \n", "Creado, mostro.\n");
+		/*Parámetros: [Path]
+		Cuando el Proceso Kernel reciba la operación de abrir un archivo deberá, en caso que el
+		archivo no exista y este sea abierto en modo de creación (“c”), llamar a esta operación que
+		creará el archivo dentro del path solicitado. Por default todo archivo creado se le debe
+		asignar al menos 1 bloque de datos*/
+		break;
+		case BORRARARCHIVO: printf("%s \n", "Borrado, papu.\n");
+		/*Parámetros: [Path]
+		Borrará el archivo en el path indicado, eliminando su archivo de metadata y marcando los
+		bloques como libres dentro del bitmap*/
+		break;
+		case OBTENERDATOS: printf("%s \n", "Tomá, comela.\n");
+		/*Parámetros: [Path, Offset, Size]
+		Ante un pedido de datos el File System devolverá del path enviado por parámetro, la
+		cantidad de bytes definidos por el Size a partir del offset solicitado. Requiere que el archivo
+		se encuentre abierto en modo lectura (“r”)*/
+		break;
+		case GUARDARDATOS: printf("%s \n", "Guardados, lince.\n");
+		/*Parámetros: [Path, Offset, Size, Buffer]
+		Ante un pedido de escritura el File System almacenará en el path enviado por parámetro, los
+		bytes del buffer, definidos por el valor del Size y a partir del offset solicitado. Requiere que el
+		archivo se encuentre abierto en modo escritura (“w”)*/
+		break;
+		default: printf("%s \n", "Numero incorrecto\n");
+		break;
+			}	/*En caso de que se soliciten datos o se intenten guardar datos en un archivo inexistente,
+				 el File System deberá retornar un error de Archivo no encontrado*/
+		}	
+ 		
+return 0;
 }
-
- 		
- 		
-
 
 
