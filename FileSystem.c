cdf00c3e07cdf5e5b8a44909fcaa807a50681725
loopmeagain <dario.fabricio.sobrino@gmail.com>
2017-07-07 19:36:03 -0300

    bitmap

--- FileSystem.c
+++ FileSystem.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <fcntl.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <unistd.h>
@@ -23,7 +24,11 @@
 #include <stdint.h>
 #include <sys/stat.h>
 #include <commons/config.h>
+#include <commons/bitarray.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 #define FILESYSTEM 84
 #define BACKLOG 5
 #define LIBRE 0
@@ -71,6 +76,7 @@
 #define PIDSIZE 1
 #define MEMORIASIZE 0
 #define VALIDARARCHIVO 58
+char * bitArray;
 
 int makeDir(char *fullpath, mode_t permissions){
 int i=0;
@@ -95,6 +101,7 @@ return 0;
 int main(){
 char* PUERTO,*PUNTO_MONTAJE;
 t_config *CFG;
+t_config * cfgMetadata;
 CFG = config_create("fileSystemCFG.txt");
 PUERTO= config_get_string_value(CFG ,"PUERTO");
 PUNTO_MONTAJE= config_get_string_value(CFG ,"PUNTO_MONTAJE");
@@ -127,7 +134,7 @@ fflush(stdout);
 sleep(1);
 printf("%s \n", "Bind OK\n");
 
-makeDir("/home/utnso/tp-2017-1c-Oreo-Triple-Crema/hola/chau",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
 
 
 freeaddrinfo(serverInfo);
@@ -140,32 +147,114 @@ printf("%s \n", "El Servidor se encuentra OK para escuchar conexiones.");
  handshakeServer(socketKernel,FILESYSTEM,(void*)unBuffer);
  t_path *path;
 
- 
+int unBit=0;
+char * direccion=calloc(1,200);
+
+bool valorBit;
+t_bitarray * bitarray;
+t_metadataFS * metadataFS=calloc(1,sizeof(t_metadataFS));
+
+strcpy(direccion,PUNTO_MONTAJE);
+
+direccion=strcat(direccion,"Metadata/Metadata.bin");
+
+/////////////////////////////////leo el metadata///////////////////////////////////////////
+cfgMetadata=config_create(direccion); 
+metadataFS->tamanioBloques=config_get_int_value(cfgMetadata,"TAMANIO_BLOQUES");printf(" tamanioBloques %i\n",metadataFS->tamanioBloques );
+metadataFS->cantBloques=config_get_int_value(cfgMetadata,"CANTIDAD_BLOQUES");printf("cantidad bloques %i\n", metadataFS->cantBloques);
+////////////////inicializo el bitarray///////////////////////////////////////////////////
+
+//no tengo idea si ya lo inicializa con todos en 0
+
+//////////////////genero el bitarray.bin/////////////////////////////////////////////////
+
+strcpy(direccion,PUNTO_MONTAJE);
+int bitmap1;
+int i;
+int confirmacion;
+int size;
+int bloques;
+if(metadataFS->cantBloques%8==0)
+{bloques=metadataFS->cantBloques;}
+else {bloques=metadataFS->cantBloques+1;}
+
+char* bmap;
+direccion=strcat(direccion,"Metadata/Bitmap.bin"); 
+
+
+ FILE * bitmap = fopen(direccion,"wb"); 
+if (bitmap!=NULL)
+{
+	fseek(bitmap, 0L, SEEK_END);
+	size = ftell(bitmap);
+	if(size<1)
+{	
+truncate(direccion,bloques/8);
+}fclose(bitmap);
+}
+	bitmap1=open(direccion,O_RDWR);printf("el bitmap es %i\n", bitmap1);
+	
+	bmap = mmap(0, bloques, PROT_WRITE | PROT_READ, MAP_SHARED, bitmap1, 0); 
+
+	if (bmap == MAP_FAILED) {
+			printf("Error al mapear a memoria: %s\n", strerror(errno));
+			close(bitmap1);
+		return 0;
+	}
+close(bitmap1);
+bitarray = bitarray_create_with_mode(bmap, bloques/8, MSB_FIRST);
+
+/*for ( i = 0; i < bloques-1; ++i)
+{	valorBit=bitarray_test_bit(bitarray,i);
+	fflush(stdout);printf("%i",(int)valorBit);
+}
+*/
+
+
+
+
+
+makeDir("/home/utnso/tp-2017-1c-Oreo-Triple-Crema/hola/chau/archivo.bin",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
 
 while(1) {
-
+path=calloc(1,sizeof(t_path));
 while(0>=recv(socketKernel,seleccionador,sizeof(t_seleccionador),0));
 
 	switch (seleccionador->tipoPaquete){
 		case VALIDARARCHIVO:
-					path=malloc(sizeof(t_path));
-					path->path=calloc(1,150);
+					
+					
 					recibirDinamico(PATH,socketKernel,path);
 					if( access( path->path, F_OK ) != -1 ) {
-						//FALTA COMUNICARSELO AL KERNEL
+						confirmacion=1;
+						send(socketKernel,&confirmacion,sizeof(int),0);
 						printf("%s \n", "Validado, troesma.\n");
 					} else {
+						confirmacion=-1;
+						send(socketKernel,&confirmacion,sizeof(int),0);
 						printf("%s \n", "El archivo no existe o no se encuentra disponible\n");
 					}
 					free(path);
 	
 		break;
-		case CREARARCHIVO: printf("%s \n", "Creado, mostro.\n");
+		case CREARARCHIVO: 
 		/*Parámetros: [Path]
+
 		Cuando el Proceso Kernel reciba la operación de abrir un archivo deberá, en caso que el
 		archivo no exista y este sea abierto en modo de creación (“c”), llamar a esta operación que
 		creará el archivo dentro del path solicitado. Por default todo archivo creado se le debe
 		asignar al menos 1 bloque de datos*/
+
+		recibirDinamico(PATH,socketKernel,path);
+		FILE * bitmap = fopen(direccion,"wb"); 
+		if (bitmap!=NULL)
+		{
+			printf("%s\n", "fue creado el archivo");
+		}
+		fclose(bitmap);
+		asignarBloques(1,&bitarray,bloques);
+
 		break;
 		case BORRARARCHIVO: printf("%s \n", "Borrado, papu.\n");
 		/*Parámetros: [Path]
