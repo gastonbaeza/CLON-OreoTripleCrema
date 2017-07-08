a956f100ffb28da43ad0674c30f67dfbafc1a792
loopmeagain <dario.fabricio.sobrino@gmail.com>
2017-07-08 07:28:24 -0300

    no compila

--- FileSystem.c
+++ FileSystem.c
@@ -77,7 +77,7 @@
 #define MEMORIASIZE 0
 #define VALIDARARCHIVO 58
 char * bitArray;
-
+char * listaBloques;
 int makeDir(char *fullpath, mode_t permissions){
 int i=0;
 char *arrDirs[20];
@@ -96,7 +96,37 @@ while(arrDirs[i]!=NULL)
 i=0;
 return 0;
 }
+int enQueBloqueEstoy(int*offset, int*tamanioBloque){
+	int numeroBloque= *offset/(*tamanioBloque);
+	return numeroBloque;
+}
 
+int deDondeEmpiezoALeer(int*offset, int*tamanioBloque){
+	int inicioLectura= offset%tamanioBloque;
+	return inicioLectura;
+}
+
+char* obtenerBloque(char**BLOQUES, int*numeroBloque){
+	char* bloque=BLOQUES[*numeroBloque];
+	return bloque;
+}
+FILE* getSiguiente(char**BLOQUES,int index){
+	char* numeroBloque=BLOQUES[index];
+	char* bloqueConBin=calloc(1,200);
+	strcpy(bloqueConBin,numeroBloque);
+	bloqueConBin=strcpy(bloqueConBin,".bin");
+	char*rutaParcial=calloc(1,200);
+	strcpy(rutaParcial,PUNTO_MONTAJE);
+	rutaParcial=strcat(rutaParcial,"Bloques/");
+	char*rutaFinal=calloc(1,200);
+	strcpy(rutaFinal,rutaParcial);
+	rutaFinal=strcat(rutaFinal,bloqueConBin);
+	FILE*f=fopen(rutaFinal,"rb");
+	free(bloqueConBin);
+	free(rutaParcial);
+	free(rutaFinal);
+	return f;
+}
 
 int main(){
 char* PUERTO,*PUNTO_MONTAJE;
@@ -156,7 +186,7 @@ t_metadataFS * metadataFS=calloc(1,sizeof(t_metadataFS));
 
 strcpy(direccion,PUNTO_MONTAJE);
 
-direccion=strcat(direccion,"Metadata/Metadata.bin");
+direccion=strcat(direccion,"/Metadata/Metadata.bin");printf("dir del metadata%s\n",direccion );
 
 /////////////////////////////////leo el metadata///////////////////////////////////////////
 cfgMetadata=config_create(direccion); 
@@ -167,19 +197,20 @@ metadataFS->cantBloques=config_get_int_value(cfgMetadata,"CANTIDAD_BLOQUES");pri
 //no tengo idea si ya lo inicializa con todos en 0
 
 //////////////////genero el bitarray.bin/////////////////////////////////////////////////
-
+printf("%s\n","comence a generar el bitarray" );
 strcpy(direccion,PUNTO_MONTAJE);
 int bitmap1;
 int i;
 int confirmacion;
 int size;
 int bloques;
+int tamBloques=metadataFS->tamanioBloques;
 if(metadataFS->cantBloques%8==0)
 {bloques=metadataFS->cantBloques;}
 else {bloques=metadataFS->cantBloques+1;}
 
 char* bmap;
-direccion=strcat(direccion,"Metadata/Bitmap.bin"); 
+direccion=strcat(direccion,"/Metadata/Bitmap.bin"); 
 
 
  FILE * bitmap = fopen(direccion,"wb"); 
@@ -188,10 +219,10 @@ if (bitmap!=NULL)
 	fseek(bitmap, 0L, SEEK_END);
 	size = ftell(bitmap);
 	if(size<1)
-{	
-truncate(direccion,bloques/8);
-}fclose(bitmap);
+	{truncate(direccion,bloques/8);}
+	fclose(bitmap);
 }
+else{printf("%s\n","que carajos esta pasando" );}
 	bitmap1=open(direccion,O_RDWR);printf("el bitmap es %i\n", bitmap1);
 	
 	bmap = mmap(0, bloques, PROT_WRITE | PROT_READ, MAP_SHARED, bitmap1, 0); 
@@ -210,17 +241,31 @@ bitarray = bitarray_create_with_mode(bmap, bloques/8, MSB_FIRST);
 }
 */
 
+char * soloDir;
+int * bloquesAsignados;
+char * pegador=calloc(1,200);
 
 
-
-
-makeDir("/home/utnso/tp-2017-1c-Oreo-Triple-Crema/hola/chau/archivo.bin",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-
-
-while(1) {
 path=calloc(1,sizeof(t_path));
-while(0>=recv(socketKernel,seleccionador,sizeof(t_seleccionador),0));
+strcpy(direccion,PUNTO_MONTAJE);
+path->path="/caca/aloha.bin";
 
+int index;
+char* bloqueDondeEstoy;
+char* bloqueConBin;
+char*rutaParcial;
+char*rutaFinal;
+int tamanio;
+char* resultado;
+int sizeAux;
+char*auxResultado;
+int valorRespuesta;
+t_config * CFGarchivo;		
+FILE*f;
+char**BLOQUES;
+t_escribirArchivo * escribirArchivoFS;
+while(0>=recv(socketKernel,seleccionador,sizeof(t_seleccionador),0));
+{
 	switch (seleccionador->tipoPaquete){
 		case VALIDARARCHIVO:
 					
@@ -245,33 +290,155 @@ while(0>=recv(socketKernel,seleccionador,sizeof(t_seleccionador),0));
 		archivo no exista y este sea abierto en modo de creación (“c”), llamar a esta operación que
 		creará el archivo dentro del path solicitado. Por default todo archivo creado se le debe
 		asignar al menos 1 bloque de datos*/
-
+		
 		recibirDinamico(PATH,socketKernel,path);
-		FILE * bitmap = fopen(direccion,"wb"); 
+
+		direccion=strcat(direccion,"/Archivos"); printf("direccion archivos %s\n",direccion );
+		direccion=strcat(direccion,path->path);
+		ultimoDirectorio(direccion,&soloDir); printf("%s\n",soloDir ); 
+
+		makeDir(soloDir,S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+		bloquesAsignados=asignarBloques(1,&bitarray,bloques);
+		listaBloques=enlistadorDeBloques(bloquesAsignados,1);
+		crearBloques(listaBloques, bitmap,PUNTO_MONTAJE);
+		strcpy(pegador,"BLOQUES=");
+		listaBloques=strcat(pegador,listaBloques);printf("lo que va en la conf %s\n",listaBloques );
+		
+		 bitmap= fopen(direccion,"ab+"); printf("final 4k %s\n",direccion );
 		if (bitmap!=NULL)
-		{
+		{	fseek(bitmap,0,SEEK_END);
+			fwrite("TAMANIO=0",strlen("TAMANIO=0"),1,bitmap);
+			fwrite("\n",1,1,bitmap);
+			fwrite(listaBloques,strlen(listaBloques),1,bitmap);
+			fclose(bitmap);
 			printf("%s\n", "fue creado el archivo");
 		}
-		fclose(bitmap);
-		asignarBloques(1,&bitarray,bloques);
+		else{perror ("Error opening threshold file");
+			printf("%s\n","se rompio algo" );}
+		
 
 		break;
 		case BORRARARCHIVO: printf("%s \n", "Borrado, papu.\n");
 		/*Parámetros: [Path]
 		Borrará el archivo en el path indicado, eliminando su archivo de metadata y marcando los
 		bloques como libres dentro del bitmap*/
+		path=malloc(sizeof(t_path));
+			recibirDinamico(PATH,socketKernel,path);
+			CFGarchivo = config_create(path->path);
+			TAMANIO= config_get_int_value(CFG ,"TAMANIO");
+			BLOQUES= config_get_int_value(CFG ,"BLOQUES");
+			cantidadBloques=cantidadElementos(BLOQUES);
+			for(i;i<cantidadBloques;i++){
+			bitarray_clean_bit(bitarray,BLOQUES[i]);
+			}
+			if( remove(path->path) != 0 )
+
+				perror("Error al borrar el archivo");
+			else
+				printf("Borrado, papu.");
+			free(path);
+			config_destroy(CFGarchivo);
 		break;
+		
 		case OBTENERDATOS: printf("%s \n", "Tomá, comela.\n");
 		/*Parámetros: [Path, Offset, Size]
 		Ante un pedido de datos el File System devolverá del path enviado por parámetro, la
 		cantidad de bytes definidos por el Size a partir del offset solicitado. Requiere que el archivo
 		se encuentre abierto en modo lectura (“r”)*/
+		leerArchivoFS=malloc(sizeof(t_leerArchivoFS));
+			recibirDinamico(LEERARCHIVOFS,socketKernel,leerArchivoFS);
+			CFGarchivo = config_create(leerArchivoFS->path);
+			TAMANIO= config_get_int_value(CFG ,"TAMANIO");
+			BLOQUES= config_get_array_value(CFG ,"BLOQUES");
+			index=enQueBloqueEstoy(leerArchivoFS->offset,tamanioBloque);
+			bloqueDondeEstoy=obtenerBloque(BLOQUES,&index);
+			bloqueConBin=calloc(1,200);
+			strcpy(bloqueConBin,bloqueDondeEstoy);
+			bloqueConBin=strcpy(bloqueConBin,".bin");
+			rutaParcial=calloc(1,200);
+			strcpy(rutaParcial,PUNTO_MONTAJE);
+			rutaParcial=strcat(rutaParcial,"Bloques/");
+			rutaFinal=calloc(1,200);
+			strcpy(rutaFinal,rutaParcial);
+			rutaFinal=strcat(rutaFinal,bloqueConBin);
+			FILE*f=fopen(rutaFinal,"rb");
+			tamanio=tamBloque-deDondeEmpiezoALeer(leerArchivoFS->offset,tamBloque);
+			fseek(f,deDondeEmpiezoALeer(leerArchivoFS->offset,tamBloque),SEEK_SET);
+			resultado=calloc(1,leerArchivoFS->size);
+			sizeAux=leerArchivoFS->size;
+			auxResultado=malloc(sizeof(leerArchivoFS->size));
+			while(sizeAux>0){
+				fread(auxResultado,tamanio,1,f);
+				resultado=strcat(resultado,auxResultado);
+				sizeAux-=tamanio;
+				if(tamBloque<sizeAux){
+					tamanio=tamBloque;
+				}else{
+					tamanio=sizeAux;
+				}
+				if(sizeAux!=0){
+					fclose(f);
+					index++;
+					f=getSiguiente(BLOQUES,index);}
+			}
+			enviarDinamico(LEERARCHIVO,socketKernel,resultado);
+			free(auxResultado);
+			free(resultado);
+			free(bloqueConBin);
+			free(rutaParcial);
+			free(rutaFinal);
+
 		break;
 		case GUARDARDATOS: printf("%s \n", "Guardados, lince.\n");
 		/*Parámetros: [Path, Offset, Size, Buffer]
 		Ante un pedido de escritura el File System almacenará en el path enviado por parámetro, los
 		bytes del buffer, definidos por el valor del Size y a partir del offset solicitado. Requiere que el
 		archivo se encuentre abierto en modo escritura (“w”)*/
+		escribirArchivoFS=calloc(1,sizeof(t_))
+		recibirDinamico(ESCRIBIRARCHIVO,socketKernel,t_escribirArchivo;);
+		CFGarchivo = config_create(escribirArchivoFS->path);
+			TAMANIO= config_get_int_value(CFG ,"TAMANIO");
+			BLOQUES= config_get_array_value(CFG ,"BLOQUES");
+	 		index=enQueBloqueEstoy(escribirArchivoFS->offset,&TAMANIO);
+			bloqueDondeEstoy=obtenerBloque(BLOQUES,index);
+			bloqueConBin=calloc(1,200);
+			strcpy(bloqueConBin,bloqueDondeEstoy);
+			bloqueConBin=strcpy(bloqueConBin,".bin");
+			rutaParcial=calloc(1,200);
+			strcpy(rutaParcial,PUNTO_MONTAJE);
+			rutaParcial=strcat(rutaParcial,"Bloques/");
+			rutaFinal=calloc(1,200);
+			strcpy(rutaFinal,rutaParcial);
+			rutaFinal=strcat(rutaFinal,bloqueConBin);
+			f=fopen(rutaFinal,"wb");
+			tamanio=tamBloque-deDondeEmpiezoALeer(escribirArchivoFS->offset,tamBloque);
+			fseek(f,deDondeEmpiezoALeer(escribirArchivoFS->offset,tamBloque),SEEK_SET);
+			sizeAux=escribirArchivoFS->size;
+			/*auxBuffer=malloc(sizeof(escribirArchivoFS->buffer));
+			auxBuffer=escribirArchivoFS->buffer;*/
+			while(sizeAux>0){
+				fwrite(auxResultado,tamanio,1,f);
+				auxResultado-=tamanio;
+				sizeAux-=tamanio;
+				if(tamBloque<sizeAux){
+					tamanio=tamBloque;
+				}else{
+					tamanio=sizeAux;
+				}
+				if(sizeAux!=0){
+					fclose(f);
+					index++;
+					f=getSiguiente(BLOQUES,&index);}
+			}
+			if(cantidadElementos(BLOQUES)*tamanioBloque<sizeAux){
+				valorRespuesta=0;
+			}else{valorRespuesta=1;}
+			send(socketKernel,&valorRespuesta,sizeof(int),0);
+			/*free(auxBuffer);*/
+			free(bloqueConBin);
+			free(rutaParcial);
+			free(rutaFinal);
+			free(escribirArchivoFS);
 		break;
 		default: printf("%s \n", "Numero incorrecto\n");
 		break;
